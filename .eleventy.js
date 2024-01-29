d50bc41cb33465d9fa7c77fd617d9d7108eb8ec4
ben-bate <e@benbate.com>
2024-01-29 09:54:16 +0000

    new images + auto experience

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    eleventyConfig.addShortcode("experience", () => `${new Date().getFullYear()-2010}`);
   };
